3026c14dc9d44948f0dc3838b912ffdbe13f7dd9
derickfan <fanderick9999@gmail.com>
2020-08-17 15:22:17 -0400

    clean up effects

--- src/components/TicTacToe/TicTacToe.js
+++ src/components/TicTacToe/TicTacToe.js
@@ -1,78 +1,62 @@
 import React, { useEffect, useState, Fragment } from "react";
-import UserDisplay from "../UserDIsplay/UserDisplay";
-import ChatDisplay from "../ChatDisplay/ChatDisplay";
 import { socket } from "../../lib/socket";
 import "./TicTacToe.css";
-import ErrorMessage from "../ErrorMessage/ErrorMessage";
 
 const TicTacToe = (props) => {
-    const [roomNumber, setRoomNumber] = useState();
     const [grid, setGrid] = useState();
-    const [roomMaster, setRoomMaster] = useState();
     const [playerTurn, setPlayerTurn] = useState();
-    const [gameError, setGameError] = useState();
+    
     const [gameStatus, setGameStatus] = useState();
 
     useEffect(() => {
-        socket.on("roomCreated", (roomNumber) => {
-            setRoomNumber(roomNumber);
-        });
-        socket.on("roomJoined", (roomNumber) => {
-            setRoomNumber(roomNumber);
-        });
-        socket.on("setRoomMaster", () => setRoomMaster(true));
+        socket.removeAllListeners('startGame');
+        socket.removeAllListeners('playerMove');
+        socket.removeAllListeners('userTurn');
+        socket.removeAllListeners('gameEnd');
+    }, []);
+    
+    useEffect(() => {
         socket.on("startGame", (grid) => {
             setGameStatus(false);
             setGrid(grid);
         });
         socket.on("playerMove", (grid) => {
-            setGameError();
             setGrid(grid);
         });
         socket.on("userTurn", (turn) => setPlayerTurn(turn));
-        socket.on("gameError", (error) => setGameError(error));
         socket.on("gameEnd", (status) => {
             setGameStatus(status);
             setPlayerTurn();
         });
-    }, []);
+    }, [grid, playerTurn, gameStatus]);
 
     const startGame = () => {
         socket.emit("startGame");
     };
 
     return (
-        <div className="room">
-            <UserDisplay gameName="TicTacToe" roomNumber={roomNumber} />
-            <div className="game-display">
-                {grid ? (
-                    <Fragment>
-                        {gameStatus ? (
-                            <h1>{gameStatus}</h1>
-                        ) : playerTurn ? (
-                            <h1>Your Turn</h1>
-                        ) : (
-                            <h1>Not Your Turn</h1>
-                        )}
-                        <Grid grid={grid} />
-                        {gameStatus && roomMaster ? (
-                            <button onClick={startGame}>New Game</button>
-                        ) : (
-                            <div></div>
-                        )}
-                    </Fragment>
-                ) : roomMaster ? (
-                    <button onClick={startGame}>Start Game</button>
-                ) : (
-                    <h1>Waiting...</h1>
-                )}
-                {gameError ? (
-                    <ErrorMessage error={gameError} setError={setGameError} />
-                ) : (
-                    <div />
-                )}
-            </div>
-            <ChatDisplay />
+        <div className="game-display">
+            {grid ? (
+                <Fragment>
+                    {gameStatus ? (
+                        <h1>{gameStatus}</h1>
+                    ) : playerTurn ? (
+                        <h1>Your Turn</h1>
+                    ) : (
+                        <h1>Not Your Turn</h1>
+                    )}
+                    <Grid grid={grid} />
+                    {gameStatus && props.roomMaster ? (
+                        <button onClick={startGame}>New Game</button>
+                    ) : (
+                        <div></div>
+                    )}
+                </Fragment>
+            ) : props.roomMaster ? (
+                <button onClick={startGame}>Start Game</button>
+            ) : (
+                <h1>Waiting...</h1>
+            )}
         </div>
     );
 };
@@ -107,9 +91,9 @@ const Cell = (props) => {
     return (
         <td onClick={playerMove}>
             {props.cell === "X" ? (
-                <i class="fas fa-times fa-6x"></i>
+                <i className="fas fa-times fa-6x"></i>
             ) : props.cell === "O" ? (
-                <i class="far fa-circle fa-6x"></i>
+                <i className="far fa-circle fa-6x"></i>
             ) : (
                 <div></div>
             )}
